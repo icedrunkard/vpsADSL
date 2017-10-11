d72a22e28472e489d79cda8e3d81808f5805910d
icedrunkard <icedrunkard@163.com>
2017-10-11 23:34:31 +0800

    subprocess redial

---
+++ redial_vps_adsl.py
@@ -0,0 +1,30 @@
+﻿import os
+import time
+def connect():
+  name="宽带连接"
+  username='75713643@hinet.net'
+  password="1qazqq"
+  cmd_str="rasdial %s %s %s" %(name,username,password)
+  res=os.system(cmd_str)
+  ipconfig=os.popen('ipconfig')
+  k=0
+  for i in ipconfig.readlines():
+    
+    print(k,i)
+    k+=1
+  if res==0:
+    print "connect successful"
+  else:
+    print res
+  time.sleep(5)
+def disconnect():
+  name="宽带连接"
+  cmdstr="rasdial %s /disconnect" %name
+  os.system(cmdstr)
+  time.sleep(5)
+
+
+while(True):
+  connect()
+  disconnect()
+  
